835de34d17708d18d595d426d6de0c1891c42fdb
khanh <khanhb1910656@student.ctu.edu.vn>
2022-10-28 20:55:46 +0700

    Create an add page

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,5 @@
 import { createWebHistory, createRouter } from "vue-router";
 import ContactBook from "@/views/ContactBook.vue";
-
 const routes = [
   {
     path: "/",
@@ -8,15 +7,25 @@ const routes = [
     component: ContactBook,
   },
   {
-    path: '/:pathMatch(.*)*',
-    name: 'notfound',
-    component: () => import('@/views/NotFound.vue'),
+    path: "/add-contact",
+    name: "contact.add",
+    component: () => import("@/views/ContactEdit.vue"),
+    props: () => ({ isAdd: true }),
+  },
+  {
+    path: "/:pathMatch(.*)*",
+    name: "notfound",
+    component: () => import("@/views/NotFound.vue"),
+  },
+  {
+    path: "/contacts/:id",
+    name: "contact.edit",
+    component: () => import("@/views/ContactEdit.vue"),
+    props: (route) => ({ contactId: parseInt(route.params.id) }),
   },
 ];
-
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes,
 });
-
 export default router;
